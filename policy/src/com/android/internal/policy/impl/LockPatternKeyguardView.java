ea6fb78e2c426617309380cc11d44cbbdb780251
Steve Kondik <shade@chemlab.org>
2012-03-13 00:04:21 -0700

    profiles: Add support for disabling screen lock
     * We can now have the option of disabling lockscreen security as part
       of the profile. A good use case for this is to create "Car" profile.
       You certainly don't want to input a pattern, or worse, face-unlock if
       you should happen to break the law and use your phone whilst driving.
     * Profile system now permits insecure mode or entirely disabling
       lockscreen as an override.
     * Fix issue where changes aren't applied if the active profile is
       modified.
     * Also fix a couple of string-comparison derps.
    
    Change-Id: I07a09b7011bad4f32cdd7bfa576e3befa71193ea

--- policy/src/com/android/internal/policy/impl/LockPatternKeyguardView.java
+++ policy/src/com/android/internal/policy/impl/LockPatternKeyguardView.java
@@ -34,6 +34,8 @@ import android.accounts.AccountManagerFuture;
 import android.accounts.AuthenticatorException;
 import android.accounts.OperationCanceledException;
 import android.app.AlertDialog;
+import android.app.Profile;
+import android.app.ProfileManager;
 import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -151,6 +153,9 @@ public class LockPatternKeyguardView extends KeyguardViewBase implements Handler
     //Also true if we've activated a phone call, either emergency dialing or incoming
     //This resets when the phone is turned off with no current call
     private boolean mHasOverlay;
+
+    // We can use the profile manager to override security
+    private ProfileManager mProfileManager;
 
 
     /**
@@ -319,6 +324,7 @@ public class LockPatternKeyguardView extends KeyguardViewBase implements Handler
         mLockPatternUtils = lockPatternUtils;
         mWindowController = controller;
         mHasOverlay = false;
+        mProfileManager = (ProfileManager) context.getSystemService(Context.PROFILE_SERVICE);
 
         mUpdateMonitor.registerInfoCallback(this);
 
@@ -847,7 +853,8 @@ public class LockPatternKeyguardView extends KeyguardViewBase implements Handler
         boolean secure = false;
         switch (unlockMode) {
             case Pattern:
-                secure = mLockPatternUtils.isLockPatternEnabled();
+                secure = mLockPatternUtils.isLockPatternEnabled() &&
+                        mProfileManager.getActiveProfile().getScreenLockMode() != Profile.LockMode.INSECURE;
                 break;
             case SimPin:
                 secure = mUpdateMonitor.getSimState() == IccCard.State.PIN_REQUIRED;
@@ -859,7 +866,8 @@ public class LockPatternKeyguardView extends KeyguardViewBase implements Handler
                 secure = true;
                 break;
             case Password:
-                secure = mLockPatternUtils.isLockPasswordEnabled();
+                secure = mLockPatternUtils.isLockPasswordEnabled() &&
+                        mProfileManager.getActiveProfile().getScreenLockMode() != Profile.LockMode.INSECURE;
                 break;
             default:
                 throw new IllegalStateException("unknown unlock mode " + unlockMode);
